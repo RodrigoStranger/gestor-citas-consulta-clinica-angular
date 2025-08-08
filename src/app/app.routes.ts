cf7080a3417b74a63e71401c035597a867c617d5
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-08-08 16:53:30 -0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MedicosComponent} from './pages/medicos/medicos';
-
+import { CalendarComponent } from './components/calendar/calendar';
 export const routes: Routes = [
   {
     path: 'medicos',
     component: MedicosComponent
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent
   }
 ];
