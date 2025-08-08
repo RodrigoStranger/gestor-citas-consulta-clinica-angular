171b461bbf4c23500aee911bc6e88bf0aad00187
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-08-08 09:52:42 -0500

    MEDICOS

---
+++ src/app/models/medico/medico.model.ts
@@ -0,0 +1,10 @@
+export interface Medico {
+  id_medico: number;
+  dni: string;
+  nombre: string;
+  apellidos: string;
+  correo: string;
+  telefono: string;
+  fechaNacimiento: Date;
+  especialidad: string;
+}
--- src/app/services/medico/medico.ts
+++ src/app/services/medico/medico.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Medico as MedicoModel } from '../../models/medico/medico.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class Medico {
+
+  url = "http://localhost:8080/api/medicos";
+
+  constructor(private readonly http:HttpClient) { }
+
+  crearMedico(medico: MedicoModel): Observable<any> {
+    return this.http.post(this.url, medico);
+  }
+
+  obtenerMedicos(estado: "habilitado" | "deshabilitado"): Observable<any>{
+    return this.http.get(`${this.url}/${estado}`);
+  }
+
+  obtenerEspecialidades(): Observable<any> {
+    return this.http.get(`${this.url}/especialidades`);
+  }
+
+  obtenerMedicoPorId(id_medico:number): Observable<any> {
+    return this.http.get(`${this.url}/${id_medico}`);
+  }
+
+  obtenerHorariosPorMedico(id_medico:number): Observable<any> {
+    return this.http.get(`${this.url}/${id_medico}/horarios`);
+  }
+
+  actualizarEspecialidadMedico(id_medico: number, especialidad: string): Observable<any> {
+    return this.http.put(`${this.url}/${id_medico}/especialidad`, { especialidad });
+  }
+
+  cambiarEstadoMedico(id_medico: number, nuevoEstado: number ): Observable<any> {
+    return this.http.put(`${this.url}/${id_medico}/estado/${nuevoEstado}`, {});
+  }
+
+
+
 
 }
